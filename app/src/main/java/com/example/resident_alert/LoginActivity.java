3ab384c978aabbcb0a4debb1bf064ae85612033d
pablo <WPawel78@gmail.com>
2020-09-28 13:23:37 +0200

    add registration, password reset and email veryfication. no errors

--- app/src/main/java/com/example/resident_alert/LoginActivity.java
+++ app/src/main/java/com/example/resident_alert/LoginActivity.java
@@ -1,14 +1,169 @@
 package com.example.resident_alert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button login_btn;
+    private EditText email_et, password_et;
+    private TextView signUp_tv,resetPassword_tv;
+    private LinearLayout forgotPassword_ll;
+
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //editText
+        email_et = (EditText) findViewById(R.id.emailRegister_et);
+        password_et = (EditText) findViewById(R.id.password_et);
+//Button
+        login_btn = (Button) findViewById(R.id.login_btn);
+//TextView
+        signUp_tv = (TextView) findViewById(R.id.noAccount_tv);
+        resetPassword_tv = (TextView) findViewById(R.id.resetPassword_tv);
+//LinearLayout
+        forgotPassword_ll = (LinearLayout) findViewById(R.id.forgotPassword_ll);
+
+//Firebase
+        fAuth = FirebaseAuth.getInstance();
+
+///////////////////////////////////////OnClick/////////////////////////////////////////////////////
+
+//login_btn
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_et.getText().toString().trim();
+                String password = password_et.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    email_et.setError("Pole nie może być puste ");
+                    email_et.requestFocus();
+                }
+                else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    email_et.setError("Pole musi być adresem e-mail");
+                    email_et.requestFocus();
+                }
+                else if(TextUtils.isEmpty(password)){
+                    password_et.setError("Pole nie może być puste");
+                    email_et.requestFocus();
+                }
+                else if(password.length()<6){
+                    password_et.setError("Hasło musi posiadać co najmniej 6 znaków");
+                    email_et.requestFocus();
+
+                }
+                else if(email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Uzupełnij pola", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    LoginUser(email,password);
+                }
+            }
+        });
+
+//SignUp_tv
+        signUp_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//resetPassword_tv
+        resetPassword_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
+            }
+        });
+
+    }
+
+///////////////////////////////////////OnStart/////////////////////////////////////////////////////
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        forgotPassword_ll.setVisibility(View.GONE);
+
+        //if user is already login
+        FirebaseUser currentUser = fAuth.getCurrentUser();
+        if(fAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),MenuActivity.class));
+            finish();
+        }
+    }
+
+///////////////////////////////////////Methods/////////////////////////////////////////////////////
+
+//Login
+private void LoginUser(String email, String password) {
+    fAuth.signInWithEmailAndPassword(email,password)
+            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(LoginActivity.this,"Witaj ! ",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+                    finish();
+                }
+
+            }).addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception e) {
+            Toast.makeText(LoginActivity.this,"Nie ma takiego konta. \n Zresetuj hasło lub stwórz nowe konto",
+                    Toast.LENGTH_LONG).show();
+            forgotPassword_ll.setVisibility(View.VISIBLE);
+
+        }
+    });
+}
+//reset password
+    private void resetPassword(){
+        String email = email_et.getText().toString().trim();
+        if (TextUtils.isEmpty(email)){
+            email_et.setError("Pole nie może być puste");
+        }
+        // mail body doesn't fit
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            email_et.setError("Pole musi być adresem E-mail");
+        }
+        else{
+            fAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(LoginActivity.this, "Wiadomość została wysłana na podany E-mail", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(LoginActivity.this, "Nie ma konta o takim E-mail" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
     }
 }
--- app/src/main/java/com/example/resident_alert/SignUpActivity.java
+++ app/src/main/java/com/example/resident_alert/SignUpActivity.java
@@ -1,14 +1,128 @@
 package com.example.resident_alert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Button signUpBtn;
+    private EditText email_et, password_et,name_et,surname_et,cnfPassword_et,tel_et,street_et,blockNumber_et,city_et,postalCode_et;
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //button
+        signUpBtn = (Button) findViewById(R.id.signup_button);
+        //edit text
+        email_et = (EditText) findViewById(R.id.emailRegister_et);
+        password_et = (EditText) findViewById(R.id.passwordRegister_et);
+        name_et = (EditText) findViewById(R.id.name_et);
+        surname_et = (EditText) findViewById(R.id.surname_et);
+        cnfPassword_et = (EditText) findViewById(R.id.cnfPassword_et);
+        tel_et = (EditText) findViewById(R.id.phone_et);
+        street_et = (EditText) findViewById(R.id.street_et);
+        blockNumber_et = (EditText) findViewById(R.id.blockNumber_et);
+        city_et = (EditText) findViewById(R.id.city_et);
+        postalCode_et = (EditText) findViewById(R.id.postalCode_et);
+
+        fAuth = FirebaseAuth.getInstance();
+
+///////////////////////////////////////OnClick/////////////////////////////////////////////////////
+
+        signUpBtn.setOnClickListener((v)->{
+            String email = email_et.getText().toString().trim();
+            String password = password_et.getText().toString().trim();
+            String cnfPassword = cnfPassword_et.getText().toString().trim();
+            String name = name_et.getText().toString().trim();
+            String surname = surname_et.getText().toString().trim();
+            String tel = tel_et.getText().toString().trim();
+            String street = street_et.getText().toString().trim();
+            String blockNumber = blockNumber_et.getText().toString().trim();
+            String city = city_et.getText().toString().trim();
+            String postalCode = postalCode_et.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)){
+                email_et.setError("Pole nie może być puste");
+                email_et.requestFocus();
+            }
+            else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                email_et.setError("Pole musi być adresem E-mail");
+                email_et.requestFocus();
+            }
+            else if (TextUtils.isEmpty(name)){
+                name_et.setError("Pole nie może być puste");
+                name_et.requestFocus();
+            }
+            else if (TextUtils.isEmpty(surname)){
+                surname_et.setError("Pole nie może być puste");
+                surname_et.requestFocus();
+            }
+            else if (TextUtils.isEmpty(tel)){
+                tel_et.setError("Pole nie może być puste");
+                tel_et.requestFocus();
+            }
+            else if (TextUtils.isEmpty(street)){
+                street_et.setError("Pole nie może być puste");
+                street_et.requestFocus();
+            }
+            else if (TextUtils.isEmpty(blockNumber)){
+                blockNumber_et.setError("Pole nie może być puste");
+                blockNumber_et.requestFocus();
+            }
+            else if (TextUtils.isEmpty(postalCode)){
+                postalCode_et.setError("Pole nie może być puste");
+                postalCode_et.requestFocus();
+            }
+
+            else if(cnfPassword.equals(password)){
+                signUp(email,password,name);
+            }
+            else{
+                cnfPassword_et.setError("Hasła muszą być takie same");
+            }
+        });
+    }
+
+    ///////////////////////////////////////Methods//////////////////////////////////////////////////
+
+    //signUp
+    private void signUp(String email, String password,String name) {
+        fAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                SignUpActivity.this.sendVerificationEmail();
+                Toast.makeText(SignUpActivity.this, "Witaj " + name ,Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+
+            }else {
+                Toast.makeText(SignUpActivity.this,"Error ! "
+                        + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    //verification email
+    private void sendVerificationEmail(){
+        FirebaseUser user = fAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnSuccessListener(aVoid ->
+                        Toast.makeText(SignUpActivity.this, "Kod weryfikacyjny został wysłany na podany E-mail", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e ->
+                        Toast.makeText(SignUpActivity.this, "error" + e.getMessage(), Toast.LENGTH_SHORT).show());
     }
 }
